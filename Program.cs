8ce09e41dd9d3e9fcd69f926c45962468b0c9870
mafik_tolik <mafik_tolik@mail.ru>
2022-10-08 00:26:21 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// Ex47();
+// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void Ex47()
+{
+    Random random = new Random();
+
+    int rows = random.Next(3, 6);
+    int columns = random.Next(3, 6);
+    Console.WriteLine($"Размер массива: {rows} * {columns}");
+
+    double[,] numbers = new double[rows, columns];
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = Math.Round((random.Next(-99, 100) * random.NextDouble()), 1);
+        }
+    }
+
+    Console.WriteLine("Вывод двумерного массива:");
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+
+Ex50();
+// Задача 50: Напишите программу, которая на вход принимает индексы элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void Ex50()
+
+{
+    int rows = 4;
+    int columns = 5;
+
+    int[,] numbers = new int[rows, columns];
+
+    // LibraryHelp.Methods.FillArray(numbers, -99, 99);
+
+    // Console.WriteLine("Введите индекс i:");
+    // int i = Convert.ToInt32(Console.ReadLine());
+
+    // Console.WriteLine("Введите индекс j:");
+    // int j = Convert.ToInt32(Console.ReadLine());
+
+    // LibraryHelp.Methods.PrintArray(numbers);
+
+    // if (i >= rows || j >= columns)
+    // {
+    //     Console.WriteLine($"Элемент с индексом ({i}, {j}) не существует в текущем массиве");
+    // }
+    // else
+    // {
+    //     Console.WriteLine($"Значение элемента с индексом ({i}, {j}) равно: {numbers[i, j]}");
+    // }
+}
+
+
+
+
+
